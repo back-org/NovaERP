3da49cde284bb58fa6bef305a314563386b4deac
RANOELISON Dimbisoa Patrick <patdimby@outlook.fr>
2025-11-27 09:45:26 +0300

    Initial commit

---
+++ src/main/java/com/novaerp/NovaerpApplication.java
@@ -0,0 +1,13 @@
+package com.novaerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaerpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novaerp/NovaerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
